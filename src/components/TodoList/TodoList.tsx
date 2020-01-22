24e187830b88f90c83f772aaf251b837210aa93a
Berkan Unal <Berkanunal@gmail.com>
2020-01-22 18:31:37 +0300

    Add filtering of todos

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -6,19 +6,34 @@ import {connect} from "react-redux";
 import {TodoItem} from "../TodoItem/TodoItem";
 import {StoreState} from "../../redux/initialState";
 
-const TodoListComponent = ({todos}: TodoListComponentProps) => (
-  <ul className="todo-list">
-    {todos.map((todoItem) => (
-      <TodoItem
-        key={todoItem.id}
-        todoItem={todoItem}
-      />
-    ))}
-  </ul>
-);
+const TodoListComponent = ({todos, currentFilter}: TodoListComponentProps) => {
+  const getVisibleTodos = () => {
+    switch (currentFilter) {
+      case "ACTIVE":
+        return todos.filter((todo) => !todo.completed);
+      case "COMPLETED":
+        return todos.filter((todo) => todo.completed);
+      case "ALL":
+      default:
+        return todos;
+    }
+  };
+
+  return (
+    <ul className="todo-list">
+      {getVisibleTodos().map((todoItem) => (
+        <TodoItem
+          key={todoItem.id}
+          todoItem={todoItem}
+        />
+      ))}
+    </ul>
+  );
+};
 
 const mapStateToProps = (storeState: StoreState) => ({
   todos: storeState.todoReducer,
+  currentFilter: storeState.filterReducer,
 });
 
 export const TodoList = connect(mapStateToProps)(TodoListComponent);
